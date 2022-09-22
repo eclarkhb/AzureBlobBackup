9130883b69f425b38abc8e27ef2712eedf60c2b2
eclarkhb <67005086+eclarkhb@users.noreply.github.com>
2022-09-22 07:08:59 -0700

    Update azureblobbackup.sh

--- azureblobbackup.sh
+++ azureblobbackup.sh
@@ -1,3 +1,27 @@
+# Before executing nsure NTP is installed & running
+# sudo yum install ntp
+# chkconfig ntpd on
+# timedatectl set-timezone America/Los_Angeles
+# ntpdate -u -s 0.centos.pool.ntp.org
+# systemctl restart ntpd
+# hwclock -w
+
+# Install Azure CLI
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+#
+# sudo sh -c 'echo -e "[azure-cli]
+# name=Azure CLI
+# baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+# enabled=1
+# gpgcheck=1
+# gpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+# sudo yum install azure-cli
+
+# Connect Azure CLI
+# az login -u UNAME@cohesityse.onmicrosoft.com -p PWORD
+
+# Begin Script
+#
 AZURE_ACCOUNT_NAME=349092cloudazure66
 AZURE_ACCOUNT_KEY=YjKz6w/l8bCn3Jrzcnp9+xy0yrjCiYoLOmYFtNOua2sQ/41mUZ8nJ2rRikidymuUphowpd3VcXiF+AStarAUgQ==
 MOUNT=/mnt/nfs/AzureBlobBackup
